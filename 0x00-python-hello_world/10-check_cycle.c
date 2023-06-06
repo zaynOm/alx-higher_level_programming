cd78722306d41c4fd25b28fde763ae44f75ae518
zaynOm <ouaziz0626@gmail.com>
2023-06-06 03:26:19 +0100

    task 10: :fire::rocket:

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,11 +10,14 @@ int check_cycle(listint_t *list)
 {
 	listint_t *head = list;
 
+	if (!list || list->next)
+		return (0);
+
 	while (list->next)
 	{
-	if (list->next == head)
-		return (1);
-	list = list->next;
+		if (list->next == head)
+			return (1);
+		list = list->next;
 	}
 
 	return (0);
