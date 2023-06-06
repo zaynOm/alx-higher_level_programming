5ec28f5932cc37cb17e0e4e1340d45aba9f8d921
zaynOm <ouaziz0626@gmail.com>
2023-06-06 03:22:51 +0100

    task 10: :fire::rocket:

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *head = list;
+
+	while (list->next)
+	{
+	if (list->next == head)
+		return (1);
+	list = list->next;
+	}
+
+	return (0);
+}
