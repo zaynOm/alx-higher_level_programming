dee660560153b56f38a35e75468cc8a3eee25931
zaynOm <ouaziz0626@gmail.com>
2023-06-08 20:23:16 +0100

    task 2: :fire:

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    ac = len(argv)
+
+    if ac == 1:
+        print('0 arguments.')
+
+    else:
+        print('{} arguments.'.format(ac - 1))
+        for i in range(1, ac):
+            print('{}: {}'.format(i, argv[i]))
