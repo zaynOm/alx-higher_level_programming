723c75c01d7319cb67a4ee656ce44f48f8ffc872
zaynOm <ouaziz0626@gmail.com>
2023-06-13 01:18:40 +0100

    task 13: :fire::rocket:

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,30 +4,22 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *node = (*head);
-	int *nums, i = 0, len = 0;
+	int nums[2048], i = 0, len = 0;
 
 	if (!*head || !(*head)->next)
 		return (1);
 
-	nums = malloc(sizeof(int));
-
 	while (node)
 	{
-		nums[len] = node->n;
+		nums[len++] = node->n;
 		node = node->next;
-		len++;
-		nums = realloc(nums, sizeof(int) * len + 1);
 	}
 
 	while (i < len)
 	{
 		if (nums[i++] != nums[(len--) - 1])
-		{
-			free(nums);
 			return (0);
-		}
 	}
-	
-	free(nums);
+
 	return (1);
 }
