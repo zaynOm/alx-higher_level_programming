6615dd2ea21415732ad1d75cbf45a4f71b89e607
zaynOm <ouaziz0626@gmail.com>
2023-06-30 00:40:16 +0100

    task 3: :fire:

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""a square class definetion"""
+
+
+class Square:
+    """this class defines a square shape.
+
+    Attributes:
+        size (int): square sides length.
+    """
+
+    def __init__(self, size=0):
+        """Initialize a new Square instance
+
+        Args:
+            size (int): square sides length, Default to 0
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is negative
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate square area
+
+        Returns:
+            int: the current square area
+        """
+        return self.__size ** 2
