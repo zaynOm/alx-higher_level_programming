0910fceac76616f297b83bfe0aadeeabd4313a15
zaynOm <ouaziz0626@gmail.com>
2023-07-05 23:37:23 +0100

    task 0: :fire:

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is "0-add_integer" module that supplies one function add_integer()"""
 
+
 def add_integer(a, b=98):
     """adds two integers
 
@@ -18,7 +19,11 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
-
-    return int(a) + int(b)
-
-
+    if a != a:
+        a = 98
+    if b != b:
+        b = 98
+    res = int(a) + int(b)
+    if res != res:
+        res = 98
+    return res
--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Multiply two matrices using numpy"""
-
-import numpy
-
-
-def lazy_matrix_mul(m_a, m_b):
-    return numpy.matmul(m_a, m_b)
