8f8f7fad8092e8b60b287a8a47bdd0ff0d443923
zaynOm <ouaziz0626@gmail.com>
2023-07-11 23:53:51 +0100

    task 0: :fire:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
         filename (str): Name of the file to read from
     """
     with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read().rstrip())
+        print(f.read(), end='')
