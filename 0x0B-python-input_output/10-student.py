33ea6d79a036d5e81ffb4d749552186db9fc8d7d
zaynOm <ouaziz0626@gmail.com>
2023-07-12 02:20:53 +0100

    task 10: :fire:

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a Student class"""
+
+
+class Student:
+    """Student class inplementation."""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance
+
+        Args:
+            first_name (str): Students first name.
+            last_name (str): Students last name.
+            age (int): Students age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation of a Student"""
+        if not attrs:
+            return self.__dict__
+        my_dict = {}
+        for item in attrs:
+            if hasattr(self, item):
+                my_dict[item] = getattr(self, item)
+        return my_dict
