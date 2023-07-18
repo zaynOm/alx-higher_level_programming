392d83f6f1865ce70f7335f59ae02d1158f000e9
zaynOm <ouaziz0626@gmail.com>
2023-07-18 02:13:20 +0100

    :fire:

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,6 +25,13 @@ class Base:
             return json.dumps(list_dictionaries)
         return '[]'
 
+    @staticmethod
+    def from_json_string(json_string):
+        "returns the list of the JSON string representation json_string"
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         "Writes the JSON string representation of list_objs to a file"
@@ -34,3 +41,26 @@ class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 f.write(Base.to_json_string(list_dicts))
+
+    @classmethod
+    def create(cls, **dictionary):
+        "returns an instance with all attributes already set"
+        if cls.__name__ == "Rectangle":
+            dum = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dum = cls(1)
+        dum.update(**dictionary)
+        return dum
+
+    @classmethod
+    def load_from_file(cls):
+        "returns a list of instances"
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as f:
+                list = cls.from_json_string(f.read())
+            for i, j in enumerate(list):
+                list[i] = cls.create(**list[i])
+            return list
+        except FileNotFoundError:
+            return []
