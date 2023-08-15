a72e7d97ea3586238ef1c8d1ba18ad42029b36b3
zaynOm <ouaziz0626@gmail.com>
2023-08-16 00:08:05 +0100

    task 9: Full creation :fire:

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- creates a table second_table in the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
-INSERT INTO TABLE second_table 
+INSERT INTO TABLE second_table (id, name, score)
 VALUES
 	(1, 'John', 10),
 	(2, 'Alex', 3),
