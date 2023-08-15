e77df04e31cc78c641126f7fdff27ac51651a3b6
zaynOm <ouaziz0626@gmail.com>
2023-08-15 23:34:29 +0100

    task 3: List tables :fire:

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,3 @@
 -- lists all the tables of a database
 -- The database name will be passed as argument of mysql command
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = DATABASE();
+SHOW TABLES;
