a28ad222b1aee1595c41910faf2f8c2df2ba272b
zaynOm <ouaziz0626@gmail.com>
2023-08-17 16:32:09 +0100

    task 9: Cities by States :fire:

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states ON cities.state_id = states.id
+SELECT c.id, c.name, s.name
+FROM cities c
+JOIN states s ON c.state_id = s.id
