9be2340f12dedd518ae5ba23dd048f4431cec611
zaynOm <ouaziz0626@gmail.com>
2023-08-18 02:57:56 +0100

    task 10: Genre ID by show :fire:

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT s.title sg.genre_id
+SELECT s.title, sg.genre_id
 FROM tv_shows s
 JOIN tv_show_genres sg ON s.id = sg.show_id
 ORDER BY s.title, sg.genre_id;
