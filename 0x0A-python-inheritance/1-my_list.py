f78ae6e4da0d08231320baacfa9f7a4752f69c98
Omar Ouaziz <63248283+zaynOm@users.noreply.github.com>
2023-09-28 23:17:37 +0100

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""A subclass of the list class with a function that prints a sorted list"""
-
-
-class MyList(list):
-    """A subclass of the list class"""
-    def print_sorted(self):
-        """Prints a list, but sorted (ascending sort)"""
-        print(sorted(self))
