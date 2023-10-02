36435f1207dfebe92230a187d481454f7745e331
zaynOm <ouaziz0626@gmail.com>
2023-10-02 23:53:11 +0100

    task 3: String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"Simple class"
+
+
+class Rectangle:
+    "Defines a rectangle class"
+
+    def __init__(self, width=0, height=0):
+        "Initialize a new rectangle instance"
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        "returns the rectangle area."
+        return self.width * self.height
+
+    def perimeter(self):
+        "returns the rectangle perimeter."
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        "Returns the rectangle represented by #"
+        if not (self.width or self.height):
+            return ''
+        return '\n'.join(['#' * self.width] * self.height)
