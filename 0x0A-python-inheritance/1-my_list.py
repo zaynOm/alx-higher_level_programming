2b46ab74d4133d978f8d69a68f72c8d2a4121b84
zaynOm <ouaziz0626@gmail.com>
2023-10-09 18:01:10 +0100

    task 1: My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"Module for MyList class"
+
+
+class MyList(list):
+    "MyList class that inherits from list"
+    def print_sorted(self):
+        "prints the list, but sorted (ascending sort)"
+        print(sorted(self))
