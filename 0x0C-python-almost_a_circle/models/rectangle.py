2396cdb5b0264e5c65dafa36bbd5c7e8f77dedbd
zaynOm <ouaziz0626@gmail.com>
2023-10-15 15:32:03 +0100

    task 6: __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,3 +85,8 @@ class Rectangle(Base):
     def display(self):
         "prints in stdout the Rectangle instance with the character #."
         print('\n'.join(['#' * self.width] * self.height))
+
+    def __str__(self):
+        "String representation of the instance."
+        return (f'[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y} - '
+                f'{self.width}/{self.height}')
