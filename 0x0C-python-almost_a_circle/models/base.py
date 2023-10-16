f20b1af30cbcd41760f0ae9a841fea6f0c2ebc3c
zaynOm <ouaziz0626@gmail.com>
2023-10-16 14:10:34 +0100

    task 15-16: Dictionary to JSON string & JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 "Module for Base class"
+import json
 
 
 class Base:
@@ -19,3 +20,30 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Dictionary to JSON string.
+
+        Args:
+            list_dictionaries (list of dict): List of dictionaries representing
+                an instance.
+
+        Returns:
+            str: JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs (list of objects): list of instances who inherits
+                of Base.
+        """
+        l_dict = [e.to_dictionary() for e in list_objs] if list_objs else None
+        with open(cls.__name__ + '.json', 'w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(l_dict))
