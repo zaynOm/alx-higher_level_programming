d49bbd3cb2e344ff6e00e4db6ec874cbb6527162
zaynom <ouaziz0626@gmail.com>
2023-11-16 13:49:53 -0800

    task 13: Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,5 @@ SELECT tg.name, COUNT(*) AS 'number_of_shows'
 FROM tv_genres tg
 JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
 GROUP BY tsg.genre_id
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
