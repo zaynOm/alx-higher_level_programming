eb425ee60d715b5e719147ed1620773320f16562
zaynom <ouaziz0626@gmail.com>
2023-11-16 17:48:44 -0800

    task 20: Best genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+-- list all genres by their rating
+SELECT tg.name, SUM(tsr.rate) 'rating'
+FROM tv_genres tg
+JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+JOIN tv_show_ratings tsr ON tsg.show_id = tsr.show_id
+GROUP BY tg.name
+ORDER BY rating DESC;
