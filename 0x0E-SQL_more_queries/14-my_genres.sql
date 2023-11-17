ed33679406c28f850ff5408aa0c6ffd961a13811
zaynom <ouaziz0626@gmail.com>
2023-11-16 16:15:00 -0800

    task 14: My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- list all genres of the show ´Dexter´
+SELECT tg.name FROM tv_genres tg
+JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+JOIN tv_shows ts ON ts.id = tsg.show_id
+WHERE ts.title = 'Dexter'
+ORDER BY tg.name;
