7439c76e36de25598760318c8f8ad82f6a54b7f8
zaynom <ouaziz0626@gmail.com>
2023-11-16 17:35:20 -0800

    task 19: Rotten tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- list all shows by their rating
+SELECT ts.title, SUM(tsr.rate) as rating
+FROM tv_shows ts
+JOIN tv_show_ratings tsr ON ts.id = tsr.show_id
+GROUP BY ts.title
+ORDER BY rating DESC;
