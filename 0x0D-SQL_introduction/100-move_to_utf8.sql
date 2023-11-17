d22de2614815fe12f2473ab0189ff4c61382f726
zaynom <ouaziz0626@gmail.com>
2023-11-16 17:57:44 -0800

    task 17: Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- converts a database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mp4 COLLATE = utf8mp4_unicode_ci;
+
+ALTER TABLE first_table CHARACTER SET = utf8mp4 COLLATE utf8mp4_unicode_ci;
