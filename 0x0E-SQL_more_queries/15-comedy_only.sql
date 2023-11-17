f157929447ecc9b0111dd174e4833113e1c8844c
zaynom <ouaziz0626@gmail.com>
2023-11-16 16:19:27 -0800

    task 15: Only Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- list all Comedy shows
+SELECT ts.title FROM tv_shows ts
+JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+JOIN tv_genres tg ON tg.id = tsg.genre_id
+WHERE tg.name = 'Comedy'
+ORDER BY ts.title;
