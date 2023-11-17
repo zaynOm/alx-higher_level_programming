e937b009ab0b19d7c0c60ff32dc73dca77985368
zaynom <ouaziz0626@gmail.com>
2023-11-16 16:31:04 -0800

    task 16: List shows and genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- list all shows and all genres linked to it
+SELECT ts.title, tg.name FROM tv_shows ts
+LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+LEFT JOIN tv_genres tg ON tg.id = tsg.genre_id
+ORDER BY ts.title, tg.name;
