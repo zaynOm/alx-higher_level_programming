517fcd7f6ae11508f833eb3f324cc4bd589b506b
zaynom <ouaziz0626@gmail.com>
2023-12-11 10:25:48 -0800

    task 2: Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
 const { argv } = require('node:process');
-
 const s = argv.length > 3 ? 's' : '';
 if (argv.length > 2) console.log(`Argument${s} found`);
 else console.log('No argument');
