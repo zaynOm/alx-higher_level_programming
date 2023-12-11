e8d579e51d2127fe9d249e68c71ecb09f6f2aefb
zaynom <ouaziz0626@gmail.com>
2023-12-11 15:34:02 -0800

    task 11: Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,5 +2,5 @@
 
 const { argv } = require('process');
 
-if (argv.length > 3) console.log(argv.slice(2).sort().reverse()[1]);
+if (argv.length > 3) console.log(argv.slice(2).sort((a, b) => b - a)[1]);
 else console.log(0);
