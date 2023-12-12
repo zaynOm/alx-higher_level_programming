b5e9cd2ea2b2d36282089680bcc358399a7ab75f
zaynom <ouaziz0626@gmail.com>
2023-12-12 14:18:54 -0800

    task 6: Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,6 @@
 const SquareBase = require('./5-square');
 
 class Square extends SquareBase {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c = 'X') {
     for (let i = 0; i < this.width; i++) {
       console.log(c.repeat(this.width));
