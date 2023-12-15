c455abcf64a921a13d4d6ead7881424a76e845f6
zaynom <ouaziz0626@gmail.com>
2023-12-15 15:56:25 -0800

    task 11: Add a new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"add new State to the database"
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine, insert
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    conn = engine.connect()
+    query = insert(State).values(name='Louisiana')
+    state = conn.execute(query)
+
+    print(state.inserted_primary_key[0])
+
+    engine.dispose()
