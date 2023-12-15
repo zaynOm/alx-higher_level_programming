9a660d92fb94b8456d1163baff03874e7bf66549
zaynom <ouaziz0626@gmail.com>
2023-12-15 08:12:21 -0800

    task 1: Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"This script lists all states with name starting with N"
+
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset='utf8')
+
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM states ORDER BY id')
+    states = cur.fetchall()
+
+    for row in states:
+        if row[1].startswith('N'):
+            print(row)
+
+    cur.close()
+    conn.close()
