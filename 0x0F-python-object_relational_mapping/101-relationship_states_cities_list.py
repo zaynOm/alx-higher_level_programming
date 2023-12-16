d8ba3d5150234c5991646f8f03b785613460cdc2
zaynom <ouaziz0626@gmail.com>
2023-12-16 08:05:57 -0800

    task 16: List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
 
+    Base.metadata.create_all(engine)
     with Session(engine) as session:
         data = session.query(State).join(City).all()
         if data:
