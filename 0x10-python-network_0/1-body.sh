bdb1218e0930a2bfea9150481645d8dcb5dafef2
zaynOm <ouaziz0626@gmail.com>
2024-01-06 22:17:48 +0100

    task 1: cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-res=$(curl -s -w "%{http_code}" "$1")
-status_code=$(echo $res | awk '{print $NF}')
-
-if [[ "$status_code" -eq 200 ]]; then
-	echo "$res" | head -1 
-fi
+curl -sL "$1"
