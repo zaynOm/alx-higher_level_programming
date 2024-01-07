ef7169575b1305b5b0dbd9b673a00b23d60480a1
zaynOm <ouaziz0626@gmail.com>
2024-01-07 23:01:26 +0100

    task 3: Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"sends a request to the URL and displays the body of the response or the error"
+from urllib import request, error
+from sys import argv
+
+if __name__ == '__main__':
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode())
+    except error.HTTPError as e:
+        print(f'Error code: {e.code}')
