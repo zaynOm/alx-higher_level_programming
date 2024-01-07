af2460dfb3fc9eee0cc8f23651c8f81498a6bbeb
zaynOm <ouaziz0626@gmail.com>
2024-01-07 23:16:21 +0100

    task 5: Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-" sends a request to the URL and displays the value of the variable X-Request-Id"
+"sends a request to a URL and displays the value of the variable X-Request-Id"
 import requests
 from sys import argv
 
