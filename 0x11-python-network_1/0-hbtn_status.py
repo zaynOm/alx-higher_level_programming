c5d9d11cd3aefd4f9ccf0b89aaabfb2959262edc
zaynOm <ouaziz0626@gmail.com>
2024-01-07 21:51:52 +0100

    task 0: What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,11 @@
 "fetch data using urllib"
 from urllib import request
 
-url = 'https://alx-intranet.hbtn.io/status'
-with request.urlopen(url) as response:
-    content = response.read()
-    print('Body response:')
-    print(f'\t- type: {type(content)}')
-    print(f'\t- content: {content}')
-    print(f'\t- utf8 content: {content.decode("utf-8")}')
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with request.urlopen(url) as response:
+        content = response.read()
+        print('Body response:')
+        print(f'\t- type: {type(content)}')
+        print(f'\t- content: {content}')
+        print(f'\t- utf8 content: {content.decode("utf-8")}')
