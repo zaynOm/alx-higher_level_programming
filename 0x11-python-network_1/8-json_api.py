32217b351438dcc5e95c43c78bd41e26f3a1a296
zaynOm <ouaziz0626@gmail.com>
2024-01-08 00:52:24 +0100

    task 8: Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,4 +7,10 @@ if __name__ == '__main__':
     if len(argv) == 2:
         data = {'q': argv[1]}
         res = requests.post('http://0.0.0.0:5000/search_user', data=data)
-        print(res.json)
+        try:
+            json = res.json()
+            print(f'[{json.get("id")}] {json.get("name")}')
+        except requests.exceptions.JSONDecodeError:
+            print('Not a valid JSON')
+    elif json == {}:
+        print('No result')
