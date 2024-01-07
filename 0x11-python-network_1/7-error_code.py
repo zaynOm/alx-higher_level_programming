c7df811ac60feaa59f810cc0ed851fb47125d7eb
zaynOm <ouaziz0626@gmail.com>
2024-01-07 23:31:31 +0100

    task 7: Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,5 +6,6 @@ from sys import argv
 if __name__ == '__main__':
     try:
         response = requests.get(argv[1])
+        response.raise_for_status()
     except requests.HTTPError as e:
         print(f'Error code: {response.status_code}')
