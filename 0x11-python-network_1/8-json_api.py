c492123a366e0e0c3ad419910a2e539f3fbcfe13
zaynOm <ouaziz0626@gmail.com>
2024-01-08 01:05:46 +0100

    task 8: Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,5 +14,5 @@ if __name__ == '__main__':
             print(f'[{json.get("id")}] {json.get("name")}')
         else:
             print('No result')
-    except requests.exceptions.JSONDecodeError:
+    except Exception:
         print('Not a valid JSON')
