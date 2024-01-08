b6bcbd07433f977a3d32c63bbc199a1325b0d0f4
zaynOm <ouaziz0626@gmail.com>
2024-01-08 01:59:49 +0100

    task 10: Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,9 +5,8 @@ from sys import argv
 
 if __name__ == '__main__':
     url = f'https://api.github.com/repos/{argv[2]}/{argv[1]}/commits'
-    headers = {'Accept': 'application/vnd.github+json'}
     payload = {'per_page': '10'}
-    res = requests.get(url, headers=headers, params=payload)
+    res = requests.get(url, params=payload)
     for commit in res.json():
         author = commit.get('commit').get('author').get('name')
         print(f"{commit.get('sha')}: {author}")
