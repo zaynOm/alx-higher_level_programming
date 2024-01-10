80f8fb82eb983738269dd8d9ab096563c54a79ee
zaynOm <ouaziz0626@gmail.com>
2024-01-11 00:36:31 +0100

    task 9: Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL, and displays the body of the response.
-curl -sLX PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me
