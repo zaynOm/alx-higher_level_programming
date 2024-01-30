9b51c2fcffb45b6bded0f782b2c8176cb4869125
zaynOm <ouaziz0626@gmail.com>
2024-01-30 18:23:54 +0100

    task 6: How many completed?

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', (err, data) => {
+fs.readFile(file, 'utf8', (err, data) => {
   if (err) throw err;
-  console.log(data + '\n');
+  console.log(data);
 });
