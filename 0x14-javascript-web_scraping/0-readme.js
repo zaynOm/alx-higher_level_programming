ff85adc3f1149218f49ca2052a04bffe82bc332b
zaynOm <ouaziz0626@gmail.com>
2024-01-30 18:07:37 +0100

    task 0: Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,6 @@ const { argv } = require('node:process');
 const { readFile } = require('node:fs');
 
 readFile(argv[2], 'utf-8', (err, data) => {
-  if (err) return console.log(err);
-  console.log(data);
+  if (err) return console.error(err);
+  console.log(data + '\n');
 });
