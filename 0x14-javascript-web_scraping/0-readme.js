7272816ea966e4ba0ff8a8bd2c0eef39d8aaae42
zaynOm <ouaziz0626@gmail.com>
2024-01-30 18:20:20 +0100

    task 0: Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const { readFile } = require('fs');
+const fs = require('fs');
 const { argv } = require('process');
 
-readFile(argv[2], 'utf-8', (err, data) => {
+fs.readFile(argv[2], 'utf-8', (err, data) => {
   if (err) throw err;
   console.log(data + '\n');
 });
