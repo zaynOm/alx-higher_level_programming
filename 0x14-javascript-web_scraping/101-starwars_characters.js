7a474af7da3954557e979dd1c0e8925902121fc1
zaynOm <ouaziz0626@gmail.com>
2024-01-30 19:13:49 +0100

    task 8: Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,13 +4,13 @@ const request = require('request');
 
 const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
 
-request(url, (err, _, body) => {
+request(url, async (err, _, body) => {
   if (err) throw err;
-  
+
   const characters = JSON.parse(body).characters;
   for (const charUrl of characters) {
-    const { name } = new Promise((resolve, reject) => {
-        request(charUrl, async (err, _, body) => {
+    const { name } = await new Promise((resolve, reject) => {
+        request(charUrl, (err, _, body) => {
           if (err) reject(err);
           resolve(JSON.parse(body));
         });
