4752cc76afa33ef1e80e51885eae72d8ad8b3e37
zaynOm <ouaziz0626@gmail.com>
2024-01-30 16:36:42 +0100

    task 5: Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const request = require('request');
+const { writeFile } = require('fs')
+
+request(argv[2], (err, _, body) => {
+  if (err) return console.log(err);
+  writeFile(argv[3], body, 'utf-8', (err) => {
+    if (err) return console.log(err);
+  });
+});
