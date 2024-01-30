f9a0753c718ceeb2428b837ca35a77cc6b4f3b58
zaynOm <ouaziz0626@gmail.com>
2024-01-30 09:59:20 +0100

    task 1: Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,5 +3,5 @@ const { argv } = require('node:process');
 const { writeFile } = require('node:fs');
 
 writeFile(argv[2], argv[3], 'utf-8', (err) => {
-    if (err) throw err;
+  if (err) throw err;
 });
