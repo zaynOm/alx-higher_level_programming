cf9ee396d87f2e30127db39f67ab580d78f78c50
zaynOm <ouaziz0626@gmail.com>
2024-01-30 18:44:36 +0100

    task 7: Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,11 +7,11 @@ const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
 request(url, (err, _, body) => {
   if (err) throw err;
   const characters = JSON.parse(body).characters;
-  for (const char_url of characters) {
-    request(char_url, (err, _, body) => {
+  for (const charUrl of characters) {
+    request(charUrl, (err, _, body) => {
       if (err) throw err;
       const person = JSON.parse(body);
       console.log(person.name);
-    })
+    });
   }
 });
