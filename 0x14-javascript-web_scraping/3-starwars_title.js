e64f81ac5c2f0362abd4575464c4686a340f9d4a
zaynOm <ouaziz0626@gmail.com>
2024-01-30 10:34:27 +0100

    task 3: Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,9 @@
 const { argv } = require('node:process');
 const request = require('request');
 
-url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`
+const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
 
-request(url, (_, res) => {
-  console.log(res.body.title);
+request(url, (err, _, body) => {
+  if (err) console.log(err);
+  console.log(JSON.parse(body).title);
 });
