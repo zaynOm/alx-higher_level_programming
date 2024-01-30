8084c5d2c4577a7b79f2bd7a299bc56156b30908
zaynOm <ouaziz0626@gmail.com>
2024-01-31 00:26:33 +0100

    task 8: Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const { argv } = require("process");
-const request = require("request");
+const { argv } = require('process');
+const request = require('request');
 
 const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
 
