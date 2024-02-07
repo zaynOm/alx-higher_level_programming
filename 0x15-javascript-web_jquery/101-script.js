ab7cf94fd01a855b501fa182fcc5fbc4e9333025
zaynOm <ouaziz0626@gmail.com>
2024-02-07 15:02:50 +0100

    task 11: List, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+$( () => {
+    $('#add_item').click(() => {
+        $('.my_list').append('<li>Item</li>')
+    })
+
+    $('#remove_item').click(() => {
+        $('.my_list li').last().remove()
+    })
+
+    $('#clear_list').click(() => {
+        $('.my_list li').remove()
+    })
+})
